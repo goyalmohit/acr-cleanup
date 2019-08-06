5bbdcff5d7c7c0c7c19437a633b54648a31fc337
Jason Loeffler <jmloeffler@gmail.com>
2019-08-06 17:16:09 -0500

    Add params for DryRun and keeping N images

--- acr-cleanup.ps1
+++ acr-cleanup.ps1
@@ -30,8 +30,40 @@ Param(
     # Gets no of days from user; images older than this will be removed
     [Parameter (Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    [String] $NoOfDays = "30"
+    [String] $NoOfDays = "30",
+
+    # Gets no of images to keep from user; images older than this will be removed
+    [Parameter (Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [String] $NoOfKeptImages,
+
+    # Allow the user to see what the script would have done, without actually deleting anything
+    [Parameter (Mandatory=$false)]
+    [bool] $DryRun = $false
 )
+
+Function Remove-Image {
+    param(
+    # Parameter help description
+    [Parameter(Mandatory=$true)]
+    [string]
+    $registryName,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $imageName, 
+    [Parameter(Mandatory=$false)]
+    [bool]
+    $dryRun=$false
+    )
+
+    if($dryRun){
+        Write-Host "Would have deleted $imageName"
+    }
+    else {
+        Write-Host "Proceeding to delete image: $imageName"
+        az acr repository delete --name $registryName --image $imageName --yes
+    }
+}
 
 Write-Host "Establishing authentication with Azure..."
 az login --service-principal -u $ServicePrincipalId -p $ServicePrincipalPass --tenant $ServicePrincipalTenant
@@ -41,33 +73,44 @@ if ($SubscriptionName){
     az account set --subscription $SubscriptionName
 }
 
-
 Write-Host "Checking registry: $AzureRegistryName"
 $RepoList = az acr repository list --name $AzureRegistryName --output table
 for($index=2; $index -lt $RepoList.length; $index++){
     $RepositoryName = $RepoList[$index]
 
     Write-Host "Checking for repository: $RepositoryName"
-    $RepositoryTags = az acr repository show-tags --name $AzureRegistryName --repository $RepositoryName --output table
+    $RepositoryTags = az acr repository show-tags --name $AzureRegistryName --repository $RepositoryName --orderby time_desc --output tsv
 
-    for($item=2; $item -lt $RepositoryTags.length; $item++){
-        $RepositoryTagName = $RepositoryTags[$item].ToString().Split('_')        
-
-        $RepositoryTagBuildDay = $RepositoryTagName[-1].ToString().Split('.')[0]
-        if($RepositoryTagBuildDay -eq "latest"){
-            Write-Host "Skipping image: $RepositoryName/latest"
-            continue;
+    # Delete by count if user specified a $NoOfKeptImages
+    if ($NoOfKeptImages -gt 0){
+        #since the list is ordered, delete the last X items
+        foreach($tag in $RepositoryTags){
+            if($RepositoryTags.IndexOf($tag) -ge $NoOfKeptImages){
+                $ImageName = $RepositoryName + ":" + $tag
+                Remove-Image -registryName $RepositoryName -imageName $ImageName -dryRun $DryRun
+            }
         }
+    }
+    # Delete by the age of the label (assuming yyyyMMdd convention in the tag names)
+    else {
+        foreach($tag in $RepositoryTags){
+            $RepositoryTagName = $tag.ToString().Split('_')        
 
-        $RepositoryTagBuildDay = [datetime]::ParseExact($repositorytagbuildday,'yyyyMMdd', $null)
-        $ImageName = $RepositoryName + ":" + $RepositoryTags[$item]
+            $RepositoryTagBuildDay = $RepositoryTagName[-1].ToString().Split('.')[0]
+            if($RepositoryTagBuildDay -eq "latest"){
+                Write-Host "Skipping image: $RepositoryName/latest"
+                continue;
+            }
 
-        if($RepositoryTagBuildDay -lt $((Get-Date).AddDays(-$NoOfDays))){
-            Write-Host "Proceeding to delete image: $ImageName"
-            az acr repository delete --name $AzureRegistryName --image $ImageName --yes
-        }        
-        else{
-            Write-Host "Skipping image: $ImageName"
+            $RepositoryTagBuildDay = [datetime]::ParseExact($repositorytagbuildday,'yyyyMMdd', $null)
+            $ImageName = $RepositoryName + ":" + $tag
+
+            if($RepositoryTagBuildDay -lt $((Get-Date).AddDays(-$NoOfDays))){
+                Remove-Image -registryName $RepositoryName -imageName $ImageName -dryRun $DryRun
+            }        
+            else{
+                Write-Host "Skipping image: $ImageName"
+            }
         }
     }
 }
